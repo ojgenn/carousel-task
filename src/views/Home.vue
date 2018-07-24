402e0ceeadfddba298789cd5ab8daea00ff09a50
ojgenn <ojgenn@gmail.comgit>
2018-07-24 16:51:51 +0300

    подготовка слайдера

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,66 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+
+        <v-text-field
+          v-model="num"
+          :rules="nameRules"
+          label="Введите количество слайдов"
+          required
+          @input="inputHandler"
+        ></v-text-field>
+        <carousel-3d :display = 'num'>
+          <slide :index="0">
+            Slide 1 Content
+          </slide>
+          <slide :index="1">
+            Slide 2 Content
+          </slide>
+          <slide :index="2">
+            Slide 3 Content
+          </slide>
+          <slide :index="3">
+            Slide 4 Content
+          </slide>
+          <slide :index="4">
+            Slide 5 Content
+          </slide>
+          <slide :index="5">
+            Slide 6 Content
+          </slide>
+          <slide :index="6">
+            Slide 7 Content
+          </slide>
+        </carousel-3d>
+        <div style="height: 3500px">
+     
+        </div>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
+
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+export default {
+  data() {
+    return{
+      num: 3,
+      nameRules: [
+        v => !!v || 'Обязательное поле',
+        v => !isNaN(v) || 'Только числа',
+        v => v <= 5 || 'Слайдов должно быть менее 5'
+      ]
+    }
+  },
+  components: {
+    Carousel3d,
+    Slide
+  }
+}
+</script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
