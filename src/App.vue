252bdb9808833a0eccc223cda81b6d53fd168acb
ojgenn <ojgenn@gmail.comgit>
2018-07-24 23:07:58 +0300

    немного beautify

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,43 @@
 <template>
   <v-app>
     <v-navigation-drawer
-      :fixed="fixed"
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      app
+            :fixed="fixed"
+            persistent
+            :mini-variant="miniVariant"
+            :clipped="clipped"
+            v-model="drawer"
+            enable-resize-watcher
+            app
     >
       <v-list>
         <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
+                value="true"
+                v-for="(item, i) in items"
+                :key="i"
         >
           <router-link :to="item.path">
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
+            <v-list-tile-action>
+              <v-icon v-html="item.icon"></v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
           </router-link>
 
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="primary"
-      :fixed="fixed"
-      app
-      :clipped-left="clipped"
+               :fixed="fixed"
+               app
+               :clipped-left="clipped"
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer" class="white--text hidden-md-and-up"></v-toolbar-side-icon>
       <v-toolbar-title class="white--text" v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
       <span v-for="(item, i) in items"
-          :key="i"
-        >
+            :key="i"
+      >
         <v-btn class="white--text hidden-sm-and-down" flat @click="goTo(item.path)">{{item.title}}</v-btn>
       </span>
     </v-toolbar>
@@ -52,41 +52,41 @@
 
 <script>
 
-export default {
-  name: 'App',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: true,
-      items: [
-        {
-          path: '/',
-          icon: 'home',
-          title: 'Home'
-        },
-        {
-          path: '/about',
-          icon: 'info',
-          title: 'About'
-        },
+  export default {
+    name: 'App',
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: true,
+        items: [
           {
-          path: '/contacts',
-          icon: 'call',
-          title: 'Contacts'
-        }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Carousel task'
-    }
-  },
-  methods: {
-    goTo(path) {
-      this.$router.push({path})
+            path: '/',
+            icon: 'home',
+            title: 'Home'
+          },
+          {
+            path: '/about',
+            icon: 'info',
+            title: 'About'
+          },
+          {
+            path: '/contacts',
+            icon: 'call',
+            title: 'Contacts'
+          }],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Carousel task'
+      }
+    },
+    methods: {
+      goTo(path) {
+        this.$router.push({path})
+      }
     }
   }
-}
 </script>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,15 @@
       <v-layout column align-center>
 
         <v-text-field
-          v-if="windowWidth > 410"
-          v-model="num"
-          :rules="numberRules"
-          label="Введите количество слайдов"
-          required
+                v-if="windowWidth > 410"
+                v-model="num"
+                :rules="numberRules"
+                label="Введите количество слайдов"
+                required
         ></v-text-field>
-        <Sliders :img = "imageArr" :num="Number(num)"/>
+        <Sliders :img="imageArr" :num="Number(num)"/>
         <div style="height: 3500px">
-     
+
         </div>
       </v-layout>
     </v-slide-y-transition>
@@ -21,43 +21,43 @@
 
 
 <script>
-import Sliders from '../components/Sliders'
-import Urls from '../assets/Urls'
+  import Sliders from '../components/Sliders'
+  import Urls from '../assets/Urls'
 
-const unsplashUrl = 'https://api.unsplash.com/photos/?client_id=207516def8772fd9bb8659348cf256a09674965e30fdf49ac376e4efc5cb2d69';
+  const unsplashUrl = 'https://api.unsplash.com/photos/?client_id=207516def8772fd9bb8659348cf256a09674965e30fdf49ac376e4efc5cb2d69';
 
-export default {
-  data() {
-    return{
-      num: 3,
-      windowWidth: window.innerWidth,
-      imageArr: [],
-      numberRules: [
-        v => !!v || 'Обязательное поле',
-        v => !isNaN(v) || 'Только числа',
-        v => v <= 5 || 'Слайдов должно быть не более 5'
-      ]
-    }
-  },
-  components: {
-    Sliders
-  }, 
-  created: function(){
-    console.log(Urls)
+  export default {
+    data() {
+      return {
+        num: 3,
+        windowWidth: window.innerWidth,
+        imageArr: [],
+        numberRules: [
+          v => !!v || 'Обязательное поле',
+          v => !isNaN(v) || 'Только числа',
+          v => v <= 5 || 'Слайдов должно быть не более 5'
+        ]
+      }
+    },
+    components: {
+      Sliders
+    },
+    created: function () {
+      console.log(Urls)
       this.imageArr = Urls
-  },
-  mounted () {
-  window.onresize = (event) => {
-    this.windowWidth = window.innerWidth;
-  };
-}
-}
+    },
+    mounted() {
+      window.onresize = (event) => {
+        this.windowWidth = window.innerWidth;
+      };
+    }
+  }
 </script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.v-text-field__slot {
+  .v-text-field__slot {
     width: 30vw;
-}
+  }
 </style>
